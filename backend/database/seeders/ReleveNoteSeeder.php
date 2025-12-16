17a3c5d97e1ad00b89a121dcf6d5bcc2f9d0db99
DouaeMoeniss <dmoeniss@emeal.nttdata.com>
2025-12-16 20:15:42 +0100

    les migrations

--- backend/database/seeders/ReleveNoteSeeder.php
+++ backend/database/seeders/ReleveNoteSeeder.php
@@ -14,21 +14,18 @@ class ReleveNoteSeeder extends Seeder
             [
                 'idDemande' => 1,
                 'annee' => '2022-2023',
-                'semestre' => 'S6',
             ],
             
             // idDemande 6 (Omar ELIDRISSI - Math L2) - En attente
             [
                 'idDemande' => 6,
                 'annee' => '2023-2024',
-                'semestre' => 'S4',
             ],
             
             // idDemande 11 (Nadia HAJJI - Master GL M1) - Validée
             [
                 'idDemande' => 11,
                 'annee' => '2023-2024',
-                'semestre' => 'S1',
             ],
         ];
 
